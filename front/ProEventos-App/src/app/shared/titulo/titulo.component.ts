41c2e8986fc551c5e0c0834f19d403920e5a5805
Danilo <danilo.silva@msn.com>
2023-04-29 23:31:35 -0300

    Melhorando o Componente Titulo

--- front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,10 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
   @Input() tituloPagina: string | undefined;
+  @Input() classeIcone: string | undefined;
+  @Input() subtituloPagina: string | undefined;
+  @Input() botaoListar: boolean | undefined;
 
   constructor() { }
 
